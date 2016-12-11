e7c58309291c0bbae7c13543f8fb679a445b59a6
Jamaotaku <Jamaotaku@gmail.com>
2016-12-11 13:24:52 +0300

    l1

---
+++ l1/Lit.java
@@ -0,0 +1,15 @@
+class LitDemo {
+	public static void main(String args[]) {
+		int n = 765;
+		long b = 67L;
+		int h = 34_347_831;
+		double a;
+		float c;
+		System.out.println("b is " + b);
+		
+		System.out.println("h is " + h);
+		
+		a = 4.5E+10;
+		System.out.println("a is " + a);
+	}
+}
---
+++ l1/Test.java
@@ -0,0 +1,8 @@
+class Test {
+	public static void main(String args[]){
+		String s = "Hello World";
+		char c = 'G';
+		System.out.println(s.length());	
+	}
+}
+
