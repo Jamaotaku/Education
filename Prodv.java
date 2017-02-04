c0d3cee6378610eb53ccd7a15001eeaa184004db
Ivaan1990 <ivaan7845@gmail.com>
2017-02-04 13:26:13 +0300

    Added dsc
    Добавил dsc.txt

---
+++ Prodv.java
@@ -0,0 +1,14 @@
+class ProdvDemo {
+	public static void main(String args[]) {
+		byte m;
+		short p;
+		int n;
+		long k;
+		m = 100;
+		p = 6856;
+		n = 65748;
+		k = 7845872;
+		p = (short) n;
+		System.out.println("p is " + p);		
+	}
+}
--- src/com/company/1.java
+++
@@ -1,6 +0,0 @@
-class Test {
-	public stattic void main(String args[]){
-		String s = "Hello World";
-		System.out.print (s)	
-	}
-}
